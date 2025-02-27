ac4d53125deb9f7b20d781c2913241ac43ce0033
Tyler452 <tpesl@csumb.edu>
2025-02-26 17:48:10 -0800

    Finished

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public delegate void EnemyDied(int points);
+    public static event EnemyDied OnEnemyDied;
+    
+    // Start is called before the first frame update
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+      Debug.Log("Ouch!");
+      Destroy(collision.gameObject);
+      
+      OnEnemyDied?.Invoke(3);
+      
+      //todo kill enemy 
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+  public GameObject bulletPrefab;
+
+  public Transform shottingOffset;
+
+  void Start()
+  {
+    Enemy.OnEnemyDied += EnemyOnOnEnemyDied;
+  }
+
+  void OnDestroy()
+  {
+    Enemy.OnEnemyDied -= EnemyOnOnEnemyDied;
+  }
+
+  void EnemyOnOnEnemyDied(int points)
+  {
+    Debug.Log($"See the hit, points: {points}");
+  }
+  
+  // Update is called once per frame
+    void Update()
+    {
+      if (Input.GetKeyDown(KeyCode.Space))
+      {
+        GameObject shot = Instantiate(bulletPrefab, shottingOffset.position, Quaternion.identity);
+        Debug.Log("Bang!");
+
+        //Destroy(shot, 3f);
+
+      }
+    }
+}
